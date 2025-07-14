29e61bb1d0b20105627bfb2d8e49e1f622a3bef1
Liuyi Lyu <lyu@Liuyis-MacBook-Air.local>
2025-07-13 18:43:05 -0700

    this is the actual 2nd version, ignore the previous ones, i messed up

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-<<<<<<< HEAD
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -9,10 +8,4 @@ export default defineConfig({
     port: 3000,
     open: true
   }
-}) 
-=======
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
 })
->>>>>>> d873b2b (2nd version, now: 1) added time gating function 2) able to delete all data or reset today's log status)
